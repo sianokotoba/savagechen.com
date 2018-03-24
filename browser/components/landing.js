e2adc2c44a65d13e945f228979f53381b52edffe
Sia <schen22@gmail.com>
2018-03-23 20:44:03 -0400

    testing out modal with materialize

--- browser/components/landing.js
+++ browser/components/landing.js
@@ -36,9 +36,25 @@ class Landing extends Component {
         </h2>
         <h3>Paradise Island, Nassau, Bahamas</h3>
         <hr />
+        { //<!-- Modal Trigger -->
+          // <button data-target="modal1" class="btn modal-trigger">Modal</button>
+        }
+        <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
         <button className="message-btn waves-effect waves-light btn-large" onClick={(e) => this.yesAttending(e)}>
           send a message
         </button>
+
+        {//<!-- Modal Structure -->
+        }
+        <div id="modal1" class="modal">
+          <div class="modal-content">
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
+          </div>
+          <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+        </div>
         {
           // <h4>Please enter the code to RSVP:</h4>
           // <div className="row">
